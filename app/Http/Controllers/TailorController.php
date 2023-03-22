1f423b2c5ce80bf77063cdc5c5b88193ea407204
Raja Ram <raja.ram@spherewms.com>
2023-03-23 01:55:11 +0500

    address removed

--- app/Http/Controllers/TailorController.php
+++ app/Http/Controllers/TailorController.php
@@ -51,7 +51,7 @@ class TailorController extends Controller
         $tailor->picture            = $request->input('picture');
         $tailor->country_id         = $request->input('country_id');
         $tailor->city_id            = $request->input('city_id');
-        $tailor->address            = $request->input('address');
+        // $tailor->address            = $request->input('address');
         $tailor->services_to_gender = $request->input('services_to_gender');
         $tailor->status             = $request->input('status');
         
