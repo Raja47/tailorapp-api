8a87a2863ff415c879128a075d5ca89b7953e311
Muteeba Ahmed <ahmedmuteeba04@gmail.com>
2024-06-03 20:10:28 +0500

    migration to load data

---
+++ database/migrations/2024_04_02_095702_insert_parameters_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\File;
+use App\Models\Parameter;
+
+class InsertParametersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        $filePath = storage_path('app/parameters.json');
+        $parameters = json_decode(File::get($filePath), true);
+
+        foreach ($parameters as $parameter) {
+            Parameter::create([
+                // 'id' => $parameter['id'],
+                'name' => $parameter['name'],
+                'label' => $parameter['label'],
+                'image' => $parameter['image'],
+                // 'is_suggested' => $parameter['is_suggested'],
+                'status' => $parameter['status'],
+                // 'createdAt' => $parameter['createdAt'],
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
