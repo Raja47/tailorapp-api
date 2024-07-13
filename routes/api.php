afc18f6c6dc1d60541aaad0a870c3b23e817b9cb
rajaram <raja.ram@shispare.com>
2024-07-13 16:21:42 +0500

    Resolving Meida Controller not Found issue

--- routes/api.php
+++ routes/api.php
@@ -139,13 +139,13 @@ Route::group(['prefix' => '/tailors/{tailor_id}/orders'], function ($router) {
     $router->post('/empty', [OrderController::class, 'emptyOrder']);
 });
 
-Route::group(['prefix' => '/media'], function ($router) {
-    $router->get('/order/{order_id}', [MediaController::class, 'getOrderMedia']);
-    $router->get('/dress/{dress_id}', [MediaController::class, 'getDressMedia']);
-    $router->post('/create', [MediaController::class, 'create']);
-    $router->post('/{media_id}/update', [MediaController::class, 'update']);
-    $router->post('/{media_id}/delete', [MediaController::class, 'delete']);
-});
+// Route::group(['prefix' => '/media'], function ($router) {
+//     $router->get('/order/{order_id}', [MediaController::class, 'getOrderMedia']);
+//     $router->get('/dress/{dress_id}', [MediaController::class, 'getDressMedia']);
+//     $router->post('/create', [MediaController::class, 'create']);
+//     $router->post('/{media_id}/update', [MediaController::class, 'update']);
+//     $router->post('/{media_id}/delete', [MediaController::class, 'delete']);
+// });
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
