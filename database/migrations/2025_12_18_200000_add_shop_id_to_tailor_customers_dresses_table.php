c9a7e93435e78b596c4dacc7e7a28721db2ccf40
raja47 <rajexhkumar123@gmail.com>
2025-12-18 21:12:49 +0500

    hotfix

--- database/migrations/2025_12_18_200000_add_shop_id_to_tailor_customers_dresses_table.php
+++ database/migrations/2025_12_18_200000_add_shop_id_to_tailor_customers_dresses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddShopIdTailorCustomersTable extends Migration
+class AddShopIdTailorToCustomersTable extends Migration
 {
     /**
      * Run the migrations.
